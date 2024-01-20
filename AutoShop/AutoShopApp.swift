81b40e699739c0898ea22811c2e52766a7ed3cc8
izzy <dom851103@gmail.com>
2024-01-20 00:28:59 -0800

    Initial Commit

---
+++ AutoShop/AutoShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoShopApp.swift
+//  AutoShop
+//
+//  Created by DOM LEUNG on 20/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct AutoShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
